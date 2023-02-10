1614fb1c71ae3e54741ac97205b43cbc83febf6d
Johnny Sin <johnnysin@Johnnys-Air.hsd1.ca.comcast.net>
2023-02-10 01:45:43 -0800

    adding all new images

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def home_page():
     return render_template('./index.html')
 
 
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
+
+
 if __name__ == '__main__':
     app.run()
