52f452c9137236d573cec9fad435c66988e62094
Johnny Sin <johnnysin@Johnnys-Air.hsd1.ca.comcast.net>
2023-02-12 23:55:00 -0800

    working edits on php form

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import csv
 
 app = Flask(__name__)
 
@@ -12,6 +13,28 @@ def home_page():
 def html_page(page_name):
     return render_template(page_name)
 
+# # Code to save contact form information to csv then later database
+# def contacts_to_csv(data):
+#     with open('database.csv', 'a', newline ='') as database:
+#         name = data['name']
+#         email = data['email']
+#         subject = data['subject']
+#         message = data['message']
+
+#         csv_writer = csv.writer(database, delimiter =',', quotechar = '"', quoting=csv.QUOTE_MINIMAL)
+#         csv_writer.writerow([email, subject, message])
+
+# @app.route('/index.html#contact', methods=['POST', 'GET'])
+# def submit_form():
+#     if request.method == 'POST':
+#         try:
+#             data = request.form.to_dict()
+#             contacts_to_csv(data)
+#             return redirect('/thankyou.html')
+#         except:
+#             return 'did not save to database'
+#     else: 
+#         return 'something went wrong. Try again'
 
 if __name__ == '__main__':
     app.run()
