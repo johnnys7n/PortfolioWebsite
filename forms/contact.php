567e32197e8e326d819f0af9f0c49127d7763f7e
Johnny Sin <johnnysin@Johnnys-Air.hsd1.ca.comcast.net>
2023-02-13 00:51:03 -0800

    finalizing commits to website

--- forms/contact.php
+++ forms/contact.php
@@ -1,13 +1,5 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
-
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'jung.sin@hotmail.com';
+  $receiving_email_address = 'junghyung.sin@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -36,6 +28,6 @@
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
-
+  $contact->honeypot = $_POST['first_name'];
   echo $contact->send();
 ?>
